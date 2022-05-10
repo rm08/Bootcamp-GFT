75e05ff549222d8534c702256abb1b1c89b65d4a
rm08 <rafael.massinatore@gmail.com>
2022-05-10 16:08:36 -0300

    Aula1 - Arrays

---
+++ src/arrays/Aula1.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		int[] vetor = {1, 2, 3, 4, 5};
+		
+		for(int i = (vetor.length - 1); i>=0; i--) {
+			System.out.println(vetor[i] + " ");
+		}
+		
+	}
+
+}
